f94d18e95547c383e510ed022692405b73c63989
Hesham Mostafa <hmostafa@Heshams-MacBook-Pro.local>
2025-09-06 18:49:27 +0300

    New Added commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,26 @@ import HomePage from "./pages/HomePage.jsx";
 import ServicesPage from "./pages/ServicesPage.jsx";
 import CasePage from "./pages/CasePage.jsx";
 import DecodeSpecialistesPage from "./pages/DecodeSpecialistesPage.jsx";
-import BlogPage from "./pages/BlogPage.jsx";  
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BlogPage from "./pages/BlogPage.jsx";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar/>
+      <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/services" element={<ServicesPage />} />
           <Route path="/case" element={<CasePage />} />
-          <Route path="/decodespecialistes" element={<DecodeSpecialistesPage />} />
+          <Route
+            path="/decodespecialistes"
+            element={<DecodeSpecialistesPage />}
+          />
           <Route path="/blog" element={<BlogPage />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
